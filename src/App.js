99e846a222debbf67c652ae8a7d27890dfa8b76b
Krishna <libraryismyparadise@gmail.com>
2022-09-17 19:16:36 +0545

    Add Nav bar to routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import CoinDetails from './pages/CoinDetails';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<CoinDetails />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
 );
