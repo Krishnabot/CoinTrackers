0aa2d43eed4fbdb0c395304bf52b5533c3d7ca8c
Krishna <libraryismyparadise@gmail.com>
2022-09-17 19:19:08 +0545

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../CSS/Footer.css';
+
+function Footer() {
+  return (
+    <footer id="footer">
+      <p>©krishna</p>
+    </footer>
+  );
+}
+
+export default Footer;
