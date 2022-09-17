e4b579c6695513dabed02c3589d34cac634b05bf
Krishna <libraryismyparadise@gmail.com>
2022-09-17 19:18:33 +0545

    Add Navigation pages

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../CSS/Home.css';
+import CoinsContainer from '../components/CoinsContainer';
+
+const Home = () => (
+  <>
+    <div className="home-wrap">
+      <h2>Track Your Coin Know How&apos;s They Doing !</h2>
+    </div>
+    <CoinsContainer />
+  </>
+);
+
+export default Home;
